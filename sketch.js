c8fba6168734fd5db5306765b5f54cc3567ca28f
Arthur Y <arthuryaucs@gmail.com>
2020-10-24 14:47:57 +0800

    Fork of moving moon animation

--- sketch.js
+++ sketch.js
@@ -1,41 +1,79 @@
-let beginX = 20.0; // Initial x-coordinate
-let beginY = 10.0; // Initial y-coordinate
-let endX = 570.0; // Final x-coordinate
-let endY = 320.0; // Final y-coordinate
-let distX; // X-axis distance to move
-let distY; // Y-axis distance to move
-let exponent = 4; // Determines the curve
-let x = 0.0; // Current x-coordinate
-let y = 0.0; // Current y-coordinate
-let step = 0.01; // Size of each step along the path
-let pct = 0.0; // Percentage traveled (0.0 to 1.0)
+var col={
+  r:0,
+  g:0,
+  b:0
+};
 
-function setup() {
-  createCanvas(720, 400);
+let Moon = 120;
+let spot={
+  x:0,
+  y:0
+};
+
+
+function setup() { 
+  createCanvas(500, 500);
+  frameRate(10);
+} 
+
+  
+function draw() { 
+  
+  //backgroundSky
+  r=map(mouseX,0,500,300,50);
+  g=map(mouseX,0,500,255,80);
+  b=map(mouseX,0,500,300,200);
+  background(r,g,b);
+  
+  
+  
+  //stars
+  spot.x=random(0,width);
+  spot.y=random(0,height);
+  comets=random(10,30);
+  speed=3
+  fill(255,255,255,100);
   noStroke();
-  distX = endX - beginX;
-  distY = endY - beginY;
-}
-
-function draw() {
-  fill(0, 2);
-  rect(0, 0, width, height);
-  pct += step;
-  if (pct < 1.0) {
-    x = beginX + pct * distX;
-    y = beginY + pow(pct, exponent) * distY;
+  star(spot.x, spot.y, 20, 50, 5); 
+  star(spot.x+150, spot.y-200, 20, 50, 5);
+  star(spot.x-300, spot.y-150, 20, 50, 5); 
+  star(spot.x+400, spot.y-300, 20, 50, 5);
+   if (spot.y>height-100){
+    speed=5;
   }
+  spot.y= spot.y+speed
+  
+  
+  //Moon
+  ellipseMode(CENTER);
+  noStroke();
   fill(255);
-  ellipse(x, y, 20, 20);
+	ellipse(mouseX,mouseY,255);
+  fill(200);
+  ellipse(mouseX+60,mouseY-5,Moon);
+   fill(200);
+  ellipse(mouseX-10,mouseY+50,30,20);
+  
+  //click
+  if(mouseIsPressed){
+   noStroke();
+  fill("#ffdb00");
+	ellipse(mouseX,mouseY,255);
+  }  
 }
 
-function mousePressed() {
-  pct = 0.0;
-  beginX = x;
-  beginY = y;
-  endX = mouseX;
-  endY = mouseY;
-  distX = endX - beginX;
-  distY = endY - beginY;
-}
+function star(x, y, radius1, radius2, npoints) {
+  var angle = TWO_PI / npoints;
+  var halfAngle = angle/2.0;
+  beginShape();
+  for (var a = 0; a < TWO_PI; a += angle) {
+    var sx = x + cos(a) * radius2;
+    var sy = y + sin(a) * radius2;
+    vertex(sx, sy);
+    sx = x + cos(a+halfAngle) * radius1;
+    sy = y + sin(a+halfAngle) * radius1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
 
+}
